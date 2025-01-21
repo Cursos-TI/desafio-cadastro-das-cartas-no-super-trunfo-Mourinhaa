f1e2b7ed44c60c5dc9f233d1c9f29ec0d85db489
Mourinhaa <thayoronne.felipe@gmail.com>
2025-01-21 14:13:39 +0000

    criei as cartas do primeiro país

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,9 +3,27 @@ int main(){
     char cidade1 [4] = "A01";  //variável para o nome da primeira cidade.
     double populacao1 = 742.452;  //variável para a população da primeira cidade.
     double area1 = 532.506;  //variável para o tamanho da primeira cidade.
-    float pib1 = 5.4;  //variável para o pib da primeira cidade.
+    float pib1 = 7;  //variável para o pib da primeira cidade.
     char turismo1 = '9';  //variável para os pontos turisticos da primeira cidade.
+    
+    char cidade2 [4] = "A02";  //variável para o nome da segunda cidade.
+    double populacao2 = 323.543;  //variável para a população da segunda cidade.
+    double area2 = 703.569;  //variável para o tamanho da segunda cidade.
+    float pib2 = 3.1;  //variável para o pib da segunda cidade cidade.
+    char turismo2 = '3';  //variável para os pontos turisticos da segunda cidade.
 
+    char cidade3 [4] = "A03";  //variável para o nome da terceira cidade.
+    double populacao3 = 254.445;  //variável para a população da terceira cidade.
+    double area3 = 913.445;  //variável para o tamanho da terceira cidade.
+    float pib3 = 9.5;  //variável para o pib da terceira cidade cidade.
+    char turismo3 = '12';  //variável para os pontos turisticos da terceira cidade.
+
+    char cidade4 [4] = "A04";  //variável para o nome da quarta cidade.
+    double populacao4 = 743.603;  //variável para a população da quarta cidade.
+    double area4 = 249.506;  //variável para o tamanho da quarta cidade.
+    float pib4 = 2.2;  //variável para o pib da quarta cidade cidade.
+    char turismo4 = '0';  //variável para os pontos turisticos da quarta cidade.
+    
     printf("Nome da cidade: \n");
     scanf("%s", &cidade1);
     printf("População: \n");
@@ -22,6 +40,53 @@ int main(){
     printf("PIB: %2.f \n", pib1);
     printf("Números de Pontos Turísticos: %c \n", turismo1);
 
+    printf("Nome da cidade: \n");
+    scanf("%s", &cidade2);
+    printf("População: \n");
+    scanf("%f", &populacao2);
+    printf("Área: \n");
+    scanf("%f", &area2);
+    printf("PIB da cidade: \n" );
+    scanf("%f", &pib2);
+    printf("Pontos Turisticos: \n");
+    scanf("%s", &turismo2);
+    printf("Cidade: %s \n", cidade2);
+    printf("População: %.3f \n", populacao2);
+    printf("Area: %.3f \n", area2);
+    printf("PIB: %2.f \n", pib2);
+    printf("Números de Pontos Turísticos: %c \n", turismo2);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", &cidade3);
+    printf("População: \n");
+    scanf("%f", &populacao3);
+    printf("Área: \n");
+    scanf("%f", &area3);
+    printf("PIB da cidade: \n" );
+    scanf("%f", &pib3);
+    printf("Pontos Turisticos: \n");
+    scanf("%s", &turismo3);
+    printf("Cidade: %s \n", cidade3);
+    printf("População: %.3f \n", populacao3);
+    printf("Area: %.3f \n", area3);
+    printf("PIB: %2.f \n", pib3);
+    printf("Números de Pontos Turísticos: %c \n", turismo3);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", &cidade4);
+    printf("População: \n");
+    scanf("%f", &populacao4);
+    printf("Área: \n");
+    scanf("%f", &area4);
+    printf("PIB da cidade: \n" );
+    scanf("%f", &pib4);
+    printf("Pontos Turisticos: \n");
+    scanf("%s", &turismo4);
+    printf("Cidade: %s \n", cidade4);
+    printf("População: %.3f \n", populacao4);
+    printf("Area: %.3f \n", area4);
+    printf("PIB: %2.f \n", pib4);
+    printf("Números de Pontos Turísticos: %c \n", turismo4);
 
 
 
